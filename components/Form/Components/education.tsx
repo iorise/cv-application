1d7c890e705357299f38c3633c8a0ff163969d51
iorise <riofirmansyah1805@gmail.com>
2023-07-04 22:36:53 +0700

    fix bug

--- components/Form/Components/education.tsx
+++ components/Form/Components/education.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import type { EducationType } from "@/types";
 import { Input } from "@/components/ui/input";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Carousel } from "react-responsive-carousel";
+import { Button } from "@/components/ui/button";
 
 type EducationProps = {
-  education: EducationType;
+  education: EducationType[];
+  addEducation: () => void;
   onChange: (
     name: string,
     value: string,
@@ -14,59 +17,85 @@ type EducationProps = {
   ) => void;
 };
 
-const Education = ({ education, onChange }: EducationProps) => {
+const Education = ({ education, onChange, addEducation }: EducationProps) => {
   const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    field: keyof EducationType
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: keyof EducationType,
+    index: number
   ) => {
     const { value } = event.target;
-    onChange(field, value, "education", 0, field);
+    onChange(field, value, "education", index, field);
   };
 
   return (
-    <div className="w-full items-center flex justify-center md:justify-start p-5">
-      <Card className="md:w-[30%] w-[90%] h-[300px]">
+
+      <Card className="">
         <CardHeader>
-          <CardTitle>Education</CardTitle>
+          <div className="flex justify-between">
+            <CardTitle>Education</CardTitle>
+            <CardTitle>
+              {education.length < 3 ? (
+                <Button variant="secondary" onClick={addEducation}>
+                  +
+                </Button>
+              ) : (
+                <Button
+                  variant="secondary"
+                  disabled={true}
+                  onClick={addEducation}
+                >
+                  +
+                </Button>
+              )}
+            </CardTitle>
+          </div>
         </CardHeader>
-        <div className="gap-1 flex flex-col">
-          <CardContent>
-            <div className="flex flex-col gap-3">
-              <Input
-                type="text"
-                placeholder="Degree"
-                value={education.degree}
-                onChange={(e) => handleInputChange(e, "degree")}
-                name="degree"
-              />
-              <Input
-                type="text"
-                placeholder="University"
-                value={education.university}
-                onChange={(e) => handleInputChange(e, "university")}
-                name="university"
-              />
-              <div className="flex justify-between gap-3">
+        <CardContent>
+          <Carousel
+            showThumbs={false}
+            showStatus={false}
+            showArrows={true}
+            showIndicators={false}
+            selectedItem={0}
+          >
+            {education.map((education, index) => (
+              <div className="flex flex-col gap-3">
                 <Input
                   type="text"
-                  placeholder="From"
-                  value={education.from}
-                  onChange={(e) => handleInputChange(e, "from")}
-                  name="from"
+                  placeholder="Degree"
+                  value={education.degree}
+                  onChange={(e) => handleInputChange(e, "degree", index)}
+                  name="degree"
                 />
                 <Input
                   type="text"
-                  placeholder="To"
-                  value={education.to}
-                  onChange={(e) => handleInputChange(e, "to")}
-                  name="to"
+                  placeholder="University"
+                  value={education.university}
+                  onChange={(e) => handleInputChange(e, "university", index)}
+                  name="university"
                 />
+                <div className="flex justify-between gap-2">
+                  <Input
+                    type="text"
+                    placeholder="From"
+                    value={education.from}
+                    onChange={(e) => handleInputChange(e, "from", index)}
+                    name="from"
+                  />
+                  <Input
+                    type="text"
+                    placeholder="To"
+                    value={education.to}
+                    onChange={(e) => handleInputChange(e, "to", index)}
+                    name="to"
+                  />
+                </div>
               </div>
-            </div>
-          </CardContent>
-        </div>
+            ))}
+          </Carousel>
+        </CardContent>
       </Card>
-    </div>
+
   );
 };
 
--- components/Form/Components/experience.tsx
+++ components/Form/Components/experience.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import React from "react";
 import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ExperienceType } from "@/types";
 import { Textarea } from "@/components/ui/textarea";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Button } from "@/components/ui/button";
 
 type ExperienceProps = {
-  experience: ExperienceType;
+  experience: ExperienceType[];
+  addExperience: () => void;
   onChange: (
     name: string,
     value: string,
@@ -15,67 +21,100 @@ type ExperienceProps = {
   ) => void;
 };
 
-const Experience = ({ experience, onChange }: ExperienceProps) => {
+const Experience = ({
+  experience,
+  addExperience,
+  onChange,
+}: ExperienceProps) => {
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    field: keyof ExperienceType
+    field: keyof ExperienceType,
+    index: number
   ) => {
     const { value } = event.target;
-    onChange(field, value, "experience", 0, field);
+    onChange(field, value, "experience", index, field);
   };
+
   return (
-    <div className="w-full items-center flex justify-center md:justify-start p-5">
-      <Card className="md:w-[30%] w-[90%] h-[300px]">
+
+      <Card className="">
         <CardHeader>
-          <CardTitle>Experience</CardTitle>
+          <div className="flex justify-between">
+            <CardTitle>Experience</CardTitle>
+            <CardTitle>
+              {experience.length < 3 ? (
+                <Button variant="secondary" onClick={addExperience}>
+                  +
+                </Button>
+              ) : (
+                <Button
+                  variant="secondary"
+                  disabled={true}
+                  onClick={addExperience}
+                >
+                  +
+                </Button>
+              )}
+            </CardTitle>
+          </div>
         </CardHeader>
         <div className="gap-1 flex flex-col">
           <CardContent>
-            <div className="flex flex-col gap-3">
-              <div className="flex justify-between">
-                <Input
-                  type="text"
-                  placeholder="Title"
-                  value={experience.title}
-                  onChange={(e) => handleInputChange(e, "title")}
-                  name="title"
-                />
-                <Input
-                  type="text"
-                  placeholder="Company"
-                  value={experience.company}
-                  onChange={(e) => handleInputChange(e, "company")}
-                  name="company"
-                />
-              </div>
-              <div className="flex justify-between">
-                <Input
-                  type="text"
-                  placeholder="From"
-                  value={experience.from}
-                  onChange={(e) => handleInputChange(e, "from")}
-                  name="from"
-                />
-                <Input
-                  type="text"
-                  placeholder="To"
-                  value={experience.to}
-                  onChange={(e) => handleInputChange(e, "to")}
-                  name="to"
-                />
-              </div>
-              <Textarea
-                className="h-[100px] resize-none"
-                placeholder="Description"
-                value={experience.description}
-                onChange={(e) => handleInputChange(e, "description")}
-                name="description"
-              />
-            </div>
+            <Carousel
+              showThumbs={false}
+              showStatus={false}
+              showArrows={true}
+              showIndicators={false}
+              selectedItem={0}
+            >
+              {experience.map((experience, index) => (
+                <div className="flex flex-col gap-3 ">
+                  <div className="flex justify-between gap-2">
+                    <Input
+                      type="text"
+                      placeholder="Title"
+                      value={experience.title}
+                      onChange={(e) => handleInputChange(e, "title", index)}
+                      name="title"
+                    />
+                    <Input
+                      type="text"
+                      placeholder="Company"
+                      value={experience.company}
+                      onChange={(e) => handleInputChange(e, "company", index)}
+                      name="company"
+                    />
+                  </div>
+                  <div className="flex justify-between">
+                    <Input
+                      type="text"
+                      placeholder="From"
+                      value={experience.from}
+                      onChange={(e) => handleInputChange(e, "from", index)}
+                      name="from"
+                    />
+                    <Input
+                      type="text"
+                      placeholder="To"
+                      value={experience.to}
+                      onChange={(e) => handleInputChange(e, "to", index)}
+                      name="to"
+                    />
+                  </div>
+                  <Textarea
+                    className="h-[100px] resize-none"
+                    placeholder="Description"
+                    value={experience.description}
+                    onChange={(e) => handleInputChange(e, "description", index)}
+                    name="description"
+                  />
+                </div>
+              ))}
+            </Carousel>
           </CardContent>
         </div>
       </Card>
-    </div>
+
   );
 };
 
