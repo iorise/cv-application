8ea39202c9710f5144133c7988efc380b4caad71
iorise <riofirmansyah1805@gmail.com>
2023-07-04 22:36:36 +0700

    refactor

--- components/Form/Components/contact.tsx
+++ components/Form/Components/contact.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 
 type ContactProps = {
   contact: CVData["contact"];
-  onChange: (name: string, value: string, section:"contact") => void;
+  onChange: (name: string, value: string, section: "contact") => void;
 };
 
 const Contact = ({ contact, onChange }: ContactProps) => {
@@ -15,49 +15,47 @@ const Contact = ({ contact, onChange }: ContactProps) => {
     onChange(name as keyof CVData, value, "contact");
   };
   return (
-    <div className="w-full items-center flex justify-center md:justify-start p-5">
-      <Card className="md:w-[30%] w-[90%] h-[300px]">
-        <CardHeader>
-          <CardTitle>Contact</CardTitle>
-        </CardHeader>
-        <div className="gap-1 flex flex-col">
-          <CardContent>
-            <div className="flex flex-col gap-3">
+    <Card className="">
+      <CardHeader>
+        <CardTitle>Contact</CardTitle>
+      </CardHeader>
+      <div className="gap-1 flex flex-col">
+        <CardContent>
+          <div className="flex flex-col gap-3">
+            <Input
+              type="text"
+              placeholder="Website"
+              value={contact.website}
+              onChange={handleInputChange}
+              name="website"
+            />
+            <Input
+              type="text"
+              placeholder="Email"
+              value={contact.email}
+              onChange={handleInputChange}
+              name="email"
+            />
+            <div className="flex justify-between gap-2">
               <Input
                 type="text"
-                placeholder="Website"
-                value={contact.website}
+                placeholder="Address"
+                value={contact.address}
                 onChange={handleInputChange}
-                name="website"
+                name="address"
               />
               <Input
                 type="text"
-                placeholder="Email"
-                value={contact.email}
+                placeholder="Phone"
+                value={contact.phone}
                 onChange={handleInputChange}
-                name="email"
+                name="phone"
               />
-              <div className="flex justify-between gap-3">
-                <Input
-                  type="text"
-                  placeholder="Address"
-                  value={contact.address}
-                  onChange={handleInputChange}
-                  name="address"
-                />
-                <Input
-                  type="text"
-                  placeholder="Phone"
-                  value={contact.phone}
-                  onChange={handleInputChange}
-                  name="phone"
-                />
-              </div>
             </div>
-          </CardContent>
-        </div>
-      </Card>
-    </div>
+          </div>
+        </CardContent>
+      </div>
+    </Card>
   );
 };
 
