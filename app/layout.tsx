db574c290ce73847d884368d6e74a624938cbabf
iorise <riofirmansyah1805@gmail.com>
2023-07-07 10:02:12 +0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import {ThemeProvider} from "@/components/theme-provider"
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'CV Application',
-  description: 'CV Application',
-}
+  title: "CV Application",
+  description: "CV Application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-          </ThemeProvider></body>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
